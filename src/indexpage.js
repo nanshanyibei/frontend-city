0f2dbf896ccf6bce8c3ee201de3004c32f4f0b60
nanshanyibei <1641685360@qq.com>
2020-03-03 21:42:53 +0800

    初步的360图片

---
+++ src/indexpage.js
@@ -0,0 +1,265 @@
+import React, { Component } from 'react';
+import './indexpage.css';
+
+class IndexPage extends Component{
+	constructor(props){
+		super(props);
+		this.state={
+
+		}
+	}
+	render(){
+		return (
+			<div className="App">
+				<div className="top-header-top">
+					<div className='left-picture'>picture1,220px*57px</div>
+					<div className="search-bar-container">
+						<div className='choose-option'>资讯</div>
+						<input className='input-area' placeholder="精品应用，海量搜索！" />
+					</div>
+					<div className='china-wuhan'>
+						中国加油！武汉加油！
+					</div>
+					<div className='login-register'>
+						<div className='login'>登录</div>
+						<div className='register'>注册</div>
+					</div>
+				</div>
+				<div className="top-header-body">
+					<div className="menu-main">
+						<span>首页</span>
+						<span>羊毛线报</span>
+						<span>长期项目</span>
+						<span>区块链技术</span>
+						<span>矿机介绍</span>
+						<span>火中取票</span>
+						<span>提现展示</span>
+						<span>热点好项目</span>
+						<span>资讯专题</span>
+					</div>
+					<div className="menu-sub">
+						<span>业务介绍</span>
+						<span>最新线报</span>
+						<span>安卓软件</span>
+						<span>新手必备</span>
+						<span>苹果软件</span>
+						<span>项目动态</span>
+						<span>网撰工具</span>
+						<span>靠谱软件</span>
+						<span>软件专题</span>
+					</div>
+				</div>
+				<div className="switch-bar">
+					<div className="word-switch">
+						<span className="red-talk">公告：</span>
+						关于合肥闪艺撒啊上飞机啦开始发六级考试发顺丰睡觉了
+					</div>
+				</div>
+				<div className="content-body">
+					<div className="the-big-first-picture">第一张图片，1160px*196px</div>
+					<div className="the-big-second-picture">第二张图片，1160px*196px</div>
+					<div className="left-big-three-picture">
+						<div className="left-big-picture">左边的大图，480px*240px</div>
+						<div className="right-three-picture">
+							<div className="right-top-three-picture">
+								<div className="first-picture">第一张图，225px*112px</div>
+								<div className="second-picture">第二张图</div>
+								<div className="thrid-picture">第三张图</div>
+							</div>
+							<div className="right-bottom-three-picture">
+								<div className="first-picture">第一张图</div>
+								<div className="second-picture">第二张图</div>
+								<div className="thrid-picture">第三张图</div>
+							</div>
+						</div>
+					</div>
+					<div className="ten-recommend">
+						<div className="top-five-recommend">
+							<div className="every-one-recommend">
+								<div className="top-word-recommend">推荐</div>
+								<div className='top-title-red-word'>好多个群啊</div>
+								<div className="middle-title-word">如何加入360官方社群？</div>
+								<div className="two-bar">
+									<span className="left-bar">qq群</span>
+									<span className="right-bar">交流问答</span>
+								</div>
+							</div>
+							<div className="every-one-recommend">
+								<div className="top-word-recommend">推荐</div>
+								<div className='top-title-red-word'>好多个群啊</div>
+								<div className="middle-title-word">如何加入360官方社群？</div>
+								<div className="two-bar">
+									<span className="left-bar">qq群</span>
+									<span className="right-bar">交流问答</span>
+								</div>
+							</div>
+							<div className="every-one-recommend">
+								<div className="top-word-recommend">推荐</div>
+								<div className='top-title-red-word'>好多个群啊</div>
+								<div className="middle-title-word">如何加入360官方社群？</div>
+								<div className="two-bar">
+									<span className="left-bar">qq群</span>
+									<span className="right-bar">交流问答</span>
+								</div>
+							</div>
+							<div className="every-one-recommend">
+								<div className="top-word-recommend">推荐</div>
+								<div className='top-title-red-word'>好多个群啊</div>
+								<div className="middle-title-word">如何加入360官方社群？</div>
+								<div className="two-bar">
+									<span className="left-bar">qq群</span>
+									<span className="right-bar">交流问答</span>
+								</div>
+							</div>
+							<div className="every-one-recommend">
+								<div className="top-word-recommend">推荐</div>
+								<div className='top-title-red-word'>好多个群啊</div>
+								<div className="middle-title-word">如何加入360官方社群？</div>
+								<div className="two-bar">
+									<span className="left-bar">qq群</span>
+									<span className="right-bar">交流问答</span>
+								</div>
+							</div>
+						</div>
+						<div className="bottom-five-recommend">
+							<div className="every-one-recommend">
+								<div className="top-word-recommend">推荐</div>
+								<div className='top-title-red-word'>好多个群啊</div>
+								<div className="middle-title-word">如何加入360官方社群？</div>
+								<div className="two-bar">
+									<span className="left-bar">qq群</span>
+									<span className="right-bar">交流问答</span>
+								</div>
+							</div>
+							<div className="every-one-recommend">
+								<div className="top-word-recommend">推荐</div>
+								<div className='top-title-red-word'>好多个群啊</div>
+								<div className="middle-title-word">如何加入360官方社群？</div>
+								<div className="two-bar">
+									<span className="left-bar">qq群</span>
+									<span className="right-bar">交流问答</span>
+								</div>
+							</div>
+							<div className="every-one-recommend">
+								<div className="top-word-recommend">推荐</div>
+								<div className='top-title-red-word'>好多个群啊</div>
+								<div className="middle-title-word">如何加入360官方社群？</div>
+								<div className="two-bar">
+									<span className="left-bar">qq群</span>
+									<span className="right-bar">交流问答</span>
+								</div>
+							</div>
+							<div className="every-one-recommend">
+								<div className="top-word-recommend">推荐</div>
+								<div className='top-title-red-word'>好多个群啊</div>
+								<div className="middle-title-word">如何加入360官方社群？</div>
+								<div className="two-bar">
+									<span className="left-bar">qq群</span>
+									<span className="right-bar">交流问答</span>
+								</div>
+							</div>
+							<div className="every-one-recommend">
+								<div className="top-word-recommend">推荐</div>
+								<div className='top-title-red-word'>好多个群啊</div>
+								<div className="middle-title-word">如何加入360官方社群？</div>
+								<div className="two-bar">
+									<span className="left-bar">qq群</span>
+									<span className="right-bar">交流问答</span>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="middle-content-word">
+						<div className="latest-updates-and-popular-articles">
+							<div className="lastest-update">
+								最新更新
+							</div>
+							<div className="every-block-report">
+								<div className="title-name">
+									羊毛线报
+								</div>
+								<div className="report-body">
+									<div className="report-body-element">
+										<div className="report-body-content">恐龙世界怎么赚钱？恐龙有钱模式不锁。。</div>
+										<div className="report-body-time">03-02</div>
+									</div>
+									<div className="report-body-element">
+										<div className="report-body-content">恐龙世界怎么赚钱？恐龙有钱模式不锁。。</div>
+										<div className="report-body-time">03-02</div>
+									</div>
+								</div>
+								<div className="report-body">
+									<div className="report-body-element">
+										<div className="report-body-content">恐龙世界怎么赚钱？恐龙有钱模式不锁。。</div>
+										<div className="report-body-time">03-02</div>
+									</div>
+									<div className="report-body-element">
+										<div className="report-body-content">恐龙世界怎么赚钱？恐龙有钱模式不锁。。</div>
+										<div className="report-body-time">03-02</div>
+									</div>
+								</div>
+							</div>
+							<div className="every-block-report">
+								<div className="title-name">
+									羊毛线报
+								</div>
+								<div className="report-body">
+									<div className="report-body-element">
+										<div className="report-body-content">恐龙世界怎么赚钱？恐龙有钱模式不锁。。</div>
+										<div className="report-body-time">03-02</div>
+									</div>
+									<div className="report-body-element">
+										<div className="report-body-content">恐龙世界怎么赚钱？恐龙有钱模式不锁。。</div>
+										<div className="report-body-time">03-02</div>
+									</div>
+								</div>
+								<div className="report-body">
+									<div className="report-body-element">
+										<div className="report-body-content">恐龙世界怎么赚钱？恐龙有钱模式不锁。。</div>
+										<div className="report-body-time">03-02</div>
+									</div>
+									<div className="report-body-element">
+										<div className="report-body-content">恐龙世界怎么赚钱？恐龙有钱模式不锁。。</div>
+										<div className="report-body-time">03-02</div>
+									</div>
+								</div>
+							</div>
+							<div className="every-block-report">
+								<div className="title-name">
+									羊毛线报
+								</div>
+								<div className="report-body">
+									<div className="report-body-element">
+										<div className="report-body-content">恐龙世界怎么赚钱？恐龙有钱模式不锁。。</div>
+										<div className="report-body-time">03-02</div>
+									</div>
+									<div className="report-body-element">
+										<div className="report-body-content">恐龙世界怎么赚钱？恐龙有钱模式不锁。。</div>
+										<div className="report-body-time">03-02</div>
+									</div>
+								</div>
+								<div className="report-body">
+									<div className="report-body-element">
+										<div className="report-body-content">恐龙世界怎么赚钱？恐龙有钱模式不锁。。</div>
+										<div className="report-body-time">03-02</div>
+									</div>
+									<div className="report-body-element">
+										<div className="report-body-content">恐龙世界怎么赚钱？恐龙有钱模式不锁。。</div>
+										<div className="report-body-time">03-02</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className="popular-articles">
+							热门文章，推荐文章
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+  
+}
+
+export default IndexPage;
+
