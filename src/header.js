8fa51090aabd6143199ba2ddc42e4cb46c409382
nanshanyibei <1641685360@qq.com>
2020-03-04 10:15:56 +0800

    昨天说的第一版页面完成

---
+++ src/header.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import './header.css';
+
+class Header extends Component{
+  constructor(props){
+		super(props);
+		this.state={
+
+		}
+	}
+	render(){
+		return (
+			<div>
+				<div className="top-header-top">
+					<div className='left-picture'>logo,220px*120px</div>
+					<div className="right-content">
+						<div className="right-top-search-login">
+							<div className="search-bar-container">
+								<div className='choose-option'>资讯</div>
+								<input className='input-area' placeholder="精品应用，海量搜索！" />
+							</div>
+							<div className='login-register'>
+								<div className='login'>登录</div>
+								<div className='register'>注册</div>
+							</div>
+						</div>
+						<div className="top-header-body">
+							<div className="menu-main">
+								<span>首页</span>
+								<span>1</span>
+								<span>2</span>
+								<span>3</span>
+								<span>4</span>
+								<span>5</span>
+								<span>6</span>
+								<span>关于</span>
+								<span>免责声明</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default Header
--- src/indexpage.js
+++ src/indexpage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Header from './header'
 import './indexpage.css';
 
 class IndexPage extends Component{
@@ -11,33 +12,7 @@ class IndexPage extends Component{
 	render(){
 		return (
 			<div className="App">
-				<div className="top-header-top">
-					<div className='left-picture'>picture1,220px*57px</div>
-					<div className="search-bar-container">
-						<div className='choose-option'>资讯</div>
-						<input className='input-area' placeholder="精品应用，海量搜索！" />
-					</div>
-					<div className='china-wuhan'>
-						中国加油！武汉加油！
-					</div>
-					<div className='login-register'>
-						<div className='login'>登录</div>
-						<div className='register'>注册</div>
-					</div>
-				</div>
-				<div className="top-header-body">
-					<div className="menu-main">
-						<span>首页</span>
-						<span>羊毛线报</span>
-						<span>长期项目</span>
-						<span>区块链技术</span>
-						<span>矿机介绍</span>
-						<span>火中取票</span>
-						<span>提现展示</span>
-						<span>热点好项目</span>
-						<span>资讯专题</span>
-					</div>
-				</div>
+				<Header />
 				<div className="content-body">
 					<div className="the-big-first-picture">第一张图片，1160px*196px</div>
 					<div className="the-big-second-picture">第二张图片，1160px*196px</div>
